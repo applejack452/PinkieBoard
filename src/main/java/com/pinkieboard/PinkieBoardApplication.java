12a279b33d71ef03c9d17c1b2e4f2d19e965b604
applejack452 <hun1323@naver.com>
2016-07-28 19:33:39 +0900

    샘플 테스트

---
+++ src/main/java/com/pinkieboard/PinkieBoardApplication.java
@@ -0,0 +1,12 @@
+package com.pinkieboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkieBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinkieBoardApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pinkieboard/PinkieBoardApplicationTests.java
@@ -0,0 +1,18 @@
+package com.pinkieboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PinkieBoardApplication.class)
+@WebAppConfiguration
+public class PinkieBoardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
